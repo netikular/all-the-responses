fa1ea36413ce7c5d2a6abb6873c78570b1f25a16
Michael Christenson II <michael@rebelhold.com>
2012-01-26 14:31:41 -0700

    set a root route

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,6 @@ RebelFoundation::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
+  root to: 'responses#respond'
   match ':controller(/:action(/:id(.:format)))' => 'responses#respond'
 end
