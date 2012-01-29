c4fc8d0951ddaba433e19b880ef6a6c14da9efe5
Kevin Pratt <kevin@ouelong.com>
2012-01-29 19:52:55 +0200

    Corrected for sending xml
    XML is sent it just echo's back the body that way you can prepare a
    document have it post and then check to make sure you got the same
    document back.

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,11 +1,10 @@
 class ResponsesController < ApplicationController
   def respond
-    debugger
     respond_to do |format|
       format.text {render text: params}
       format.js   {render text: params.to_json}
       format.json {render text: params.to_json}
-      format.xml  {render text: params.to_xml}
+      format.xml  {render text: request.body.read}
       format.html {}
     end
   end
